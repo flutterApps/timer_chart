39812099aaabf487505f41951db523578c3a38f0
netsun <303078098@qq.com>
2020-04-21 19:21:57 +0800

    no message

--- lib/entity/period_entity.dart
+++ lib/entity/period_entity.dart
@@ -4,13 +4,16 @@ class PeriodEntity {
   int type;
   int openTime;
   int closeTime;
-  List<TsEntity> tss = [];
+  List<TsEntity> tss;
 
   PeriodEntity({
+    this.type,
     this.openTime,
     this.closeTime,
     this.tss,
   });
+
+
 
   @override
   String toString() {
